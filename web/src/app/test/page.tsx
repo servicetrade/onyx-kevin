37315d743777e0d7eb4e18faf8e8dfb9d17d3b23
pablodanswer <pablo@danswer.ai>
2025-01-20 11:11:11 -0800

    nit

---
+++ web/src/app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <textarea name="input47" rows={3} cols={10} className="draggable-textarea">
+      Your multiline text is here.
+    </textarea>
+  );
+}
