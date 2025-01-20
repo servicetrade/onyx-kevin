54b7f300044f388c1244ae9db2936b855562f2aa
pablodanswer <pablo@danswer.ai>
2025-01-20 11:21:00 -0800

    finalize chat ux

--- web/src/app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Test() {
-  return (
-    <textarea name="input47" rows={3} cols={10} className="draggable-textarea">
-      Your multiline text is here.
-    </textarea>
-  );
-}
