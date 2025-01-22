a621069a6552980b30d4927406d3902741e31461
pablodanswer <pablo@danswer.ai>
2025-01-22 12:39:19 -0800

    update auth

--- web/src/app/auth/signup/page.tsx
+++ web/src/app/auth/signup/page.tsx
@@ -60,6 +60,7 @@ const Page = async (props: {
     return redirect("/chat");
   }
 
+  console.log(authTypeMetadata);
   let authUrl: string | null = null;
   if (cloud && authTypeMetadata) {
     authUrl = await getAuthUrlSS(authTypeMetadata.authType, null);
