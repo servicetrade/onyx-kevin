2ce1389f88c614ea3616342aedebd321fd8f55f5
pablodanswer <pablo@danswer.ai>
2025-01-29 16:35:43 -0800

    ensure sidepanel defaults sidepanel off

--- web/src/app/chat/WrappedChat.tsx
+++ web/src/app/chat/WrappedChat.tsx
@@ -5,14 +5,18 @@ import FunctionalWrapper from "../../components/chat/FunctionalWrapper";
 
 export default function WrappedChat({
   firstMessage,
+  defaultSidebarOff,
 }: {
   firstMessage?: string;
+  // This is required for the chrome extension side panel
+  // we don't want to show the sidebar by default when the user opens the side panel
+  defaultSidebarOff?: boolean;
 }) {
   const { toggledSidebar } = useChatContext();
 
   return (
     <FunctionalWrapper
-      initiallyToggled={toggledSidebar}
+      initiallyToggled={toggledSidebar && !defaultSidebarOff}
       content={(toggledSidebar, toggle) => (
         <ChatPage
           toggle={toggle}
