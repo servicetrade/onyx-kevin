16102e6b2d0a1195de5a4fa0efcf08b479082f39
pablodanswer <pablo@danswer.ai>
2025-02-01 11:29:41 -0800

    improve timeout

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   globalSetup: require.resolve("./tests/e2e/global-setup"),
-  timeout: 60000, // 60 seconds timeout
+  timeout: 600000, // 10 minutes timeout
   projects: [
     {
       name: "admin",
