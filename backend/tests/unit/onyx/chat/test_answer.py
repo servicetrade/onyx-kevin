8e41ca9a9fe3e9a5c418b8d269eb9cbc72498cb4
Evan Lohn <evan@danswer.ai>
2025-03-31 09:31:29 -0700

    fixed unit test

--- backend/tests/unit/onyx/chat/test_answer.py
+++ backend/tests/unit/onyx/chat/test_answer.py
@@ -323,9 +323,12 @@ def test_answer_with_search_no_tool_calling(
         == mock_search_tool.build_next_prompt.return_value.build.return_value
     )
 
+    user_message = (
+        answer_instance.graph_inputs.prompt_builder.get_user_message_content()
+    )
     # Verify that get_args_for_non_tool_calling_llm was called on the mock_search_tool
     mock_search_tool.get_args_for_non_tool_calling_llm.assert_called_once_with(
-        QUERY, [], answer_instance.graph_config.tooling.primary_llm
+        user_message, [], answer_instance.graph_config.tooling.primary_llm
     )
 
     # Verify that the search tool's run method was called
